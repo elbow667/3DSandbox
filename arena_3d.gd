598c91fb182fd2f31b0c6624d8f0447a33226f7d
elbow667 <elbow667@gmail.com>
2023-04-21 22:40:54 -0400

    Respawn code added

--- arena_3d.gd
+++ arena_3d.gd
@@ -8,9 +8,18 @@ var tps = preload("res://Characters/player_2.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spawn_player()
+
+
+
+# Player falls or gets thrown off the platform
+func _on_area_3d_body_exited(body):
+	body.position = player_spawn_point
+	body.rotation = player_spawn_rotation
+#	var _reload = get_tree().reload_current_scene()
+
+func spawn_player():
 	var player = tps.instantiate()
 	add_child(player)
 	player.position = player_spawn_point
 	player.rotation = player_spawn_rotation
-	$SpawnPoint/MeshInstance3D.visible = false
-
